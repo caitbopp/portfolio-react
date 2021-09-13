6b5a6bda97306872f2c612500ea13c80e703990f
Caitlin Bopp <boppcaitlin@gmail.com>
2021-09-12 21:37:46 -0400

    Populated Footer.js, NavBar.js, and PortfolioContainer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Footer() {
+    return (
+        <div className="footer text-center">
+            <p><a href="#">410.868.2677</a> | <a href="mailto:boppcaitlin@gmail.com">boppcaitlin@gmail.com</a> | <a href="https://github.com/caitbopp">GitHub</a> | <a href="https://linkedin.com/in/caitlinbopp">LinkedIn</a>
+            </p>
+        </div>
+
+    );
+}
---
+++ src/components/NavBar.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import resume from '../assets/resume.pdf';
+
+function NavBar({ currentPage, handlePageChange }) {
+    return (<header>
+        <nav className="navbar navbar-expand-lg">
+            <div className="container-fluid">
+                <h1 className="navbar-brand">Caitlin Bopp</h1>
+                <div className="collapse navbar-collapse  d-flex flex-sm-row flex-lg-row-reverse" id="navbarNav">
+                    <ul className="nav nav-tabs">
+                        <li className="nav-item">
+                            <a href="#about" onClick={() => handlePageChange('About Me')} className={currentPage === 'About Me' ? 'nav-link active' : 'nav-link'}>
+                                About Me
+                            </a>
+                        </li>
+                        <li className="nav-item">
+                            <a href="#portfolio" onClick={() => handlePageChange('Portfolio')} className={currentPage === 'Portfolio' ? 'nav-link active' : 'nav-link'}>
+                                Portfolio
+                            </a>
+                        </li>
+                        <li className="nav-item">
+                            <a href={resume} className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}>
+                                Resume
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>)
+};
+
+
+export default NavBar;
