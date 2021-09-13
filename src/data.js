2def745f933059ec704909193aced6f1ab19c96b
Caitlin Bopp <boppcaitlin@gmail.com>
2021-09-12 21:40:23 -0400

    Created and populated data.js file withint src folder

---
+++ src/data.js
@@ -0,0 +1,43 @@
+import booleanBookworm from './assets/project2-screenshot.png';
+import yodaAdvice from './assets/project-screenshot.png';
+import workDayScheduler from './assets/work-day-scheduler.png';
+import teamProfileGenerator from './assets/team-profile-generator.png';
+
+export const projects = [
+    {
+      title: "Boolean Bookworm",
+      subtitle: "TEST",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: booleanBookworm,
+      deployedLink: "https://hidden-refuge-01270.herokuapp.com/",
+      gitHubLink: "https://github.com/pgurung95/Boolean_Bookworm",
+    },
+    {
+      title: "Yoda Advice",
+      subtitle: "TEST",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: yodaAdvice,
+      deployedLink: "https://caitbopp.github.io/yoda-advice/",
+      gitHubLink: "https://github.com/caitbopp/yoda-advice",
+    },
+    {
+      title: "Work Day Scheduler",
+      subtitle: "TEST",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: workDayScheduler,
+      deployedLink: "https://caitbopp.github.io/work-day-scheduler/",
+      gitHubLink: "https://github.com/caitbopp/work-day-scheduler",
+    },
+    {
+      title: "Team Profile Generator",
+      subtitle: "TEST",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: teamProfileGenerator,
+      deployedLink: "https://youtu.be/ZxebSb95PSU",
+      gitHubLink: "https://github.com/caitbopp/team-profile-generator",
+    },
+  ];
